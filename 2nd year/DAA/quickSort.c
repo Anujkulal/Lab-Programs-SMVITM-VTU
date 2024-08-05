5a104f87cbc454ba8bd38c394fd665fda4265a3b
anujkulal <anujkulal333@gmail.com>
2024-08-05 22:16:24 +0530

    commit

--- 2nd year/DAA/quickSort.c
+++ 2nd year/DAA/quickSort.c
@@ -4,10 +4,9 @@
 #include<math.h>
 #include<string.h>
 #include<time.h>
-
 int partition(int arr[], int low, int high){
     int pivot = arr[high];
-    int i = low - 1;
+    int i = low-1;
     for(int j=low; j<high; j++){
         if(arr[j] <= pivot){
             i++;
@@ -22,33 +21,26 @@ int partition(int arr[], int low, int high){
     return i+1;
 }
 
-void quickSort(int arr[], int low, int high){
+void quicksort(int arr[], int low, int high){
     if(low < high){
         int pi = partition(arr, low, high);
-        quickSort(arr, low, pi-1);
-        quickSort(arr, pi+1, high);
+        quicksort(arr, low, pi-1);
+        quicksort(arr, pi+1, high);
     }
 }
-
 int main() {
     int n;
     printf("Enter the number of elements: ");
     scanf("%d", &n);
-    int arr[n];
     srand(time(NULL));
-    printf("Enter the elements: ");
+    int arr[n];
     for(int i=0; i<n; i++){
-        // arr[i] = rand() % 10;
-        scanf("%d", &arr[i]);
+        arr[i] = rand() % 1000;
     }
     clock_t start = clock();
-    quickSort(arr, 0, n-1);
+    quicksort(arr, 0, n-1);
     clock_t end = clock();
     double timeTaken = ((double)(end - start))/CLOCKS_PER_SEC;
-    printf("Time taken for sorting %f\n", timeTaken);
-    for(int i=0; i<n; i++){
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
+    printf("Time taken for sorting: %f\n", timeTaken);
     return 0;
 }
